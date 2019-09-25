0b8cf029e49a6623b9ae48fbf0656ab107a23581
csalley95 <c1salley@student.bridgew.edu>
2019-09-25 11:59:25 -0400

    Can exit successfully.

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank {
     private ArrayList<BankAccount> accounts;
@@ -11,7 +12,26 @@ public class Bank {
         clients = new ArrayList<Customer>();
     }
 
+    private void printMenu(){
+        System.out.println("Please select an option below: ");
+        System.out.println(" [1] Add a customer");
+        System.out.println(" [2] Select a Customer for banking");
+        System.out.println(" [3] Remove Customer");
+        System.out.println(" [4] Do yearly Maintenance");
+        System.out.println(" [5] Exit Program");
+        System.out.print("Type the number of your choice: ");
+    }
     public void doBanking(){
-        System.out.println("Calling ");
+        var reader = new Scanner(System.in);
+        while (true){
+            printMenu();
+            var userSelection = reader.nextInt();
+            switch(userSelection){
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("I don't know how to do that yet.");
+            }
+        }
     }
 }
