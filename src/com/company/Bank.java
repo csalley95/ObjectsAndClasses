d8214e698dc60cebaf179ee01f8145c45958b660
csalley95 <c1salley@student.bridgew.edu>
2019-09-25 11:33:11 -0400

    Main1

---
+++ src/com/company/Bank.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private ArrayList<BankAccount> accounts;
+    private ArrayList<Customer> clients;
+
+    public Bank(){
+        accounts = new ArrayList<BankAccount>();
+        clients = new ArrayList<Customer>();
+    }
+
+    public void doBanking(){
+        System.out.println("Calling ");
+    }
+}
